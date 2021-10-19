14754641bead43c4f20d4fa09e12b21419e01057
ibv6789 <88753487+ibv6789@users.noreply.github.com>
2021-10-18 22:52:38 -0400

    Update FAQ.vue

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -246,7 +246,7 @@
                   <b>Can I arrive earlier than 1 PM on Saturday?</b>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                Yes, you can arrive earlier on Saturday. (For example, some overnight guests might want to get situated in their cabins early.) But there won't be any 
+                Yes, you can arrive earlier on Saturday. (For example, some overnight guests might want to get settled in their cabins.) But there won't be any 
                 official events or meals until 1 PM.
                 </v-expansion-panel-content>
               </v-expansion-panel>
