a6e41d55b70d8125b445eb0890da2a52ea61ba8d
Stephen Suffian <stephen.suffian@pm.me>
2022-02-09 20:48:06 -0500

    Updated gifts

--- src/views/Gifts.vue
+++ src/views/Gifts.vue
@@ -11,22 +11,32 @@
               We are very privileged to have everything we need & more for our
               future together. We also recognize that many guests are taking on
               considerable expense and time to travel to our wedding. Thank you
-              so much for your presence and generosity! Our biggest wish for our wedding is for our family
-              and friends to connect and create a sense of community together. Your
-              openess to meeting strangers and active participation will be the most meaningful contributions.
-          
+              so much for your presence and generosity! Our biggest wish for our
+              wedding is for our family and friends to connect and create a
+              sense of community together. Your openess to meeting strangers and
+              active participation will be the most meaningful contributions.
             </p>
 
             <p>
               For those interested in giving in other ways, we would be honored
               if our wedding could play a small role in supporting the important
               work of community organizations and social justice movements. If
-              you would like to make a donation in honor of our wedding, please consider these causes which are meaningful to us:
+              you would like to make a donation in honor of our wedding, please
+              consider these causes which are meaningful to us:
             </p>
             <v-container>
               <ul>
                 <li>
-                <a href="https://www.why-not-prosper.org/" target="_blank"><b>Why Not Prosper</b></a> is a grassroots organization supporting formerly incarcerated women in Philadelphia. <a href="https://www.why-not-prosper.org/donate.html" target="_blank">Click here to donate</a>.
+                  <a href="https://www.why-not-prosper.org/" target="_blank"
+                    ><b>Why Not Prosper</b></a
+                  >
+                  is a grassroots organization supporting formerly incarcerated
+                  women in Philadelphia.
+                  <a
+                    href="https://www.why-not-prosper.org/donate.html"
+                    target="_blank"
+                    >Click here to donate</a
+                  >.
                 </li>
                 <li>
                   <a
@@ -69,14 +79,12 @@
                   >
                 </li>
                 <li>
-                  <a
-                    href="https://unrepresented.wordpress.com/"
-                    target="_blank"
+                  <a href="https://unrepresented.wordpress.com/" target="_blank"
                     ><b>The Unrepresented Death Row Prisoner Project</b></a
                   >
-                 provides necessary items (stamps, hygiene products, clothing) for people under sentence of death in
-                  California who are waiting for a lawyer to be appointed to
-                  their case. 
+                  provides necessary items (stamps, hygiene products, clothing)
+                  for people under sentence of death in California who are
+                  waiting for a lawyer to be appointed to their case.
                   <a
                     href="https://unrepresented.wordpress.com/donations/"
                     target="_blank"
@@ -84,7 +92,17 @@
                   >
                 </li>
                 <li>
-                <a href="https://sogoreate-landtrust.org/" target="_blank"><b>The Sogorea Te’ Land Trust</b></a> is an urban Indigenous women-led land trust centered in Huchiun, in unceded Lisjan (Ohlone) territory, now known as the East Bay in the San Francisco Bay Area. <a href="https://sogoreate-landtrust.org/donate/" target="_blank">Click here to donate</a>.
+                  <a href="https://nllhonorarium.carrd.co/" target="_blank"
+                    ><b>The Lenapehoking Land Acknowledgement Honorarium</b></a
+                  >
+                  supports the Nanticoke Lenni-Lenape Tribal Nation, whose
+                  ancestors inhabited the land encompassed in Lenapehoking
+                  (including NJ, northern DE, eastern PA and southeastern NY).
+                  <a
+                    href="https://www.paypal.com/paypalme/nllhonorarium"
+                    target="_blank"
+                    >Click here to donate</a
+                  >.
                 </li>
               </ul>
             </v-container>
@@ -92,8 +110,8 @@
             <p>
               If you feel it’s important to give us a gift directly, you can
               send us a gift via Venmo <b>@beckymmer</b> or
-              <b>@stephensuffian</b>. You can also mail a check (written to
-              both of us, “Rebecca Mer or Stephen Suffian”) to
+              <b>@stephensuffian</b>. You can also mail a check (written to both
+              of us, “Rebecca Mer or Stephen Suffian”) to
               <b>1567 Gregory Drive, Warrington, PA 18976</b>. This is Steve’s
               parents’ address, where all gifts can be sent.
             </p>
@@ -105,7 +123,10 @@
                 target="_blank"
                 ><b>created a registry</b></a
               >
-              for new items and gift cards. We'd also like to prioritize second-hand gifts. If you see a registry item similar to something you have but aren't using, and would like to give as a second-hand gift, feel free to email us directly.  
+              for new items and gift cards. We'd also like to prioritize
+              second-hand gifts. If you see a registry item similar to something
+              you have but aren't using, and would like to give as a second-hand
+              gift, feel free to email us directly.
             </p>
 
             <p>Thank you so much!</p>
@@ -118,6 +139,6 @@
 
 <script>
 export default {
-  components: {}
+  components: {},
 };
 </script>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -9,37 +9,62 @@
             <v-img src="img/pages/schedule.jpeg" max-height="300"></v-img><br />
             <p>
               Our wedding ceremony will be on <b>Sunday, May 29, 2022</b>.
-              <span v-if="wholeWeekend">Celebrate with us for the entire weekend!</span> As we get closer to the
-              wedding, this page will be updated with more details. All events take place at Camp Laurelwood.
+              <span v-if="wholeWeekend"
+                >Celebrate with us for the entire weekend!</span
+              >
+              As we get closer to the wedding, this page will be updated with
+              more details. All events take place at Camp Laurelwood.
             </p>
             <div v-if="wholeWeekend">
-            <h2>Saturday May 28, 2022</h2>
-            Wedding weekend begins!<br><br>
-            <b>1 PM - </b> Lunch at camp<br>
-            <b>After Lunch -</b> Optional camp activities (canoeing, swimming, hammocks, swingsets, gaga courts, games, sports)<br>
-            <b>4 PM -</b> Meet and Greet event<br>
-            <b>6 PM -</b> Welcome Dinner BBQ<br>
-            <b>8 PM - </b>Family-friendly talent show (featuring you, our guests!)<br>
-            <b>9 PM - </b>Bonfire and Karaoke
-            </div><br>
+              <h2>Saturday May 28, 2022</h2>
+              Wedding weekend begins!<br /><br />
+              <b>1 PM - </b> Lunch at camp (food served in Mess Hall, outdoor
+              seating available)<br />
+              <b>After Lunch -</b> Optional camp activities (canoeing, swimming,
+              hammocks, swingsets, gaga courts, games, sports)<br />
+              <b>4 PM -</b> Meet and Greet event (outside,
+              weather-permitting)<br />
+              <b>6 PM -</b> Welcome Dinner BBQ (outside, weather-permitting)<br />
+              <b>8 PM - </b>Family-friendly talent show (featuring you, our
+              guests!) (outside, weather-permitting)<br />
+              <b>9 PM - </b>Bonfire and Karaoke
+            </div>
+            <br />
             <h2>Sunday May 29, 2022</h2>
-            <span v-if="wholeWeekend"><b>11 AM - </b> Brunch at camp<br></span>
-            <span v-if="wholeWeekend"><b>After Brunch -</b> Optional camp activities (canoeing, swimming, hammocks, swingsets, gaga courts, games, sports)<br></span>
-            <b>3:30 PM</b> Wedding ceremony begins! The wedding ceremony  will include:
+            <span v-if="wholeWeekend"
+              ><b>11 AM - </b> Brunch at camp (food served in Mess Hall, outdoor
+              seating available)<br
+            /></span>
+            <span v-if="wholeWeekend"
+              ><b>After Brunch -</b> Optional camp activities (canoeing,
+              swimming, hammocks, swingsets, gaga courts, games, sports)<br
+            /></span>
+            <b>3:30 PM</b> Wedding ceremony begins! The wedding ceremony will
+            include:
             <v-container
               ><ul>
                 <li>
-                  Tisches (separate, joyful gatherings for Becky and Steve)
+                  Tisches: separate, joyful gatherings for Becky (outside,
+                  weather-permitting) and Steve (in Mess Hall)
                 </li>
-                <li>Signing the ketubah (Jewish marriage contract)</li>
-                <li>A ceremony under the chuppah</li>
-                <li>A dinner reception, plus lots of dancing</li>
+                <li>
+                  Signing the ketubah (Jewish marriage contract) (outside,
+                  weather-permitting)
+                </li>
+                <li>
+                  A ceremony under the chuppah (outside, weather-permitting)
+                </li>
+                <li>
+                  A dinner reception, plus lots of dancing (outside,
+                  weather-permitting)
+                </li>
               </ul>
             </v-container>
-            Later in the evening, there will be an after-party and bonfire.<br><br>
+            Later in the evening, there will be an after-party and bonfire.<br /><br />
 
             <h2>Monday May 30, 2022</h2>
-            <b>9:00 AM - 11 AM - </b> Goodbye Brunch at camp<br>
+            <b>9:00 AM - 11 AM - </b> Goodbye Brunch at camp (food served in
+            Mess Hall, outdoor seating available)<br />
           </div>
         </div>
       </section>
@@ -49,10 +74,10 @@
 
 <script>
 export default {
-    data() { 
-        return { 
-        wholeWeekend: localStorage.wholeWeekend === 'true' 
-    }
-    }
+  data() {
+    return {
+      wholeWeekend: localStorage.wholeWeekend === "true",
+    };
+  },
 };
 </script>
