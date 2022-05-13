c436f84274e6218162757190820131f7cfd99fe2
Stephen Suffian <stephen.suffian@pm.me>
2022-05-12 22:28:19 -0400

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,28 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6
+        maxChunks: 6,
       }),
-    ]
+    ],
   },
-  pwa: {
+  /*pwa: {
     name: 'Wedding Website',
     themeColor: '#172b4d',
     msTileColor: '#172b4d',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: '#172b4d'
-  },
+  },*/
 
   css: {
     // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
+    sourceMap: process.env.NODE_ENV !== "production",
   },
 
-  outputDir: 'dist/',
+  outputDir: "dist/",
 
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+};
