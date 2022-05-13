8130d42432c48b47faebf2a4308117411ee762fe
Stephen Suffian <stephen.suffian@pm.me>
2022-05-12 22:33:57 -0400

    Revert "Update"
    This reverts commit c436f84274e6218162757190820131f7cfd99fe2.

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,14 @@ module.exports = {
       }),
     ],
   },
+  pwa: {
+    workboxOptions: { skipWaiting: true },
+    name: "Wedding Website",
+    themeColor: "#172b4d",
+    msTileColor: "#172b4d",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#172b4d",
+  },
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== "production",
