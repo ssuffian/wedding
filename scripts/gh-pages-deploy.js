fccf914e30f24718d9df09b8037f3b5ae482ebd1
Stephen Suffian <stephen.suffian@pm.me>
2022-05-14 20:44:31 -0400

    Update

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -6,7 +6,7 @@ const fs = require("fs");
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
-    await execa("npm", ["run", "build", "--mode", "development"]);
+    await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("cp", ["CNAME", folderName]);
